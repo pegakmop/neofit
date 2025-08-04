aee910669d35f2e128008f1324d7ba1dff21906e
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-08-04 19:40:10 +0300

    Create sing-box-go-remove.sh

---
+++ sing-box-go-remove.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# === Installer NeoFit by @pegakmop ===
+
+HRNEO_DIR="/opt/share/www/sing-box-go"
+INDEX_FILE="$HRNEO_DIR/index.php"
+MANIFEST_FILE="$HRNEO_DIR/manifest.json"
+LIGHTTPD_CONF_DIR="/opt/etc/lighttpd/conf.d"
+LIGHTTPD_CONF_FILE="$LIGHTTPD_CONF_DIR/80-sing-box-go.conf"
+ip_addres=$(ip addr show br0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+echo ""
+echo "Начинаем удаление NeoFit WebUI..."
+echo ""
+echo "[*] Останавливаем процессы"
+/opt/etc/init.d/S80lighttpd stop
+echo ""
+echo "[*] Удаление установленных пакетов..."
+opkg remove lighttpd lighttpd-mod-cgi lighttpd-mod-setenv lighttpd-mod-redirect lighttpd-mod-rewrite php8 php8-cgi php8-cli php8-mod-curl php8-mod-openssl sing-box-go jq
+echo ""
+echo "[*] Удаление директорий..."
+rm -rf "$HRNEO_DIR"
+rm -rf "$LIGHTTPD_CONF_DIR"
+echo ""
+echo "[*] Удаление с автозагрузки..."
+rm -rf /opt/etc/init.d/S80lighttpd rm -rf /opt/bin/neofitweb
+echo ""
+echo "[*] Аннигилятор веб панели удален."
+rm "$0"
+echo ""
+echo "[*] NeoFit WebUi removed"
